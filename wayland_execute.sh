093bcd298f80649cc7dc7cf730b09ee1e79418a6
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2025-12-03 22:42:11 -0600

    borrar todote

--- wayland_execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Forzando el uso del driver de video SDL: X11"
-export SDL_VIDEODRIVER=x11
-export KIVY_GL_VSYNC=0
-
-mangohud --dlsym vblank_mode=0 python3 main.py
-
-echo "Ejecución de Kivy finalizada."
