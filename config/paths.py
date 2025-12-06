90e771324607397819ffd0c3716cebb4cd675016
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2025-12-05 22:16:09 -0600

    mejoras

---
+++ config/paths.py
@@ -0,0 +1,13 @@
+from utils import ResourceLoader
+
+resource_loader = ResourceLoader()
+TEMP_DIR = resource_loader.base_dir.joinpath('tmp')
+AUDIO_DIR = resource_loader.resources_dir.joinpath( 'audio' )
+TEMPO_DIR = AUDIO_DIR.joinpath( 'tempo' )
+SAMPLE_DIR = AUDIO_DIR.joinpath( 'sample' )
+
+DICT_SAMPLE_DIR = resource_loader.get_recursive_tree( SAMPLE_DIR )
+SAMPLE_FILES = DICT_SAMPLE_DIR['file']
+
+DICT_TEMPO_DIR = resource_loader.get_recursive_tree( TEMPO_DIR )
+TEMPO_FILES = DICT_TEMPO_DIR["file"]
