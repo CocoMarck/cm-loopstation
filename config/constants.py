422a66c5255f1821dd9844d499375e94141c1eec
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-02-26 22:18:39 -0600

    better about, better kv string

---
+++ config/constants.py
@@ -0,0 +1,4 @@
+NAME = "FPSLoopstation"
+VERSION = "0.3.6-alpha"
+DEVELOPER = "Jean Abraham Chacón Candanosa"
+WEBSITE = "github.com/CocoMarck"
--- config/paths.py
+++ config/paths.py
@@ -5,6 +5,7 @@ TEMP_DIR = resource_loader.base_dir.joinpath('tmp')
 AUDIO_DIR = resource_loader.resources_dir.joinpath( 'audio' )
 TEMPO_DIR = AUDIO_DIR.joinpath( 'tempo' )
 SAMPLE_DIR = AUDIO_DIR.joinpath( 'sample' )
+VIEWS_DIR = resource_loader.base_dir.joinpath('views')
 
 DICT_SAMPLE_DIR = resource_loader.get_recursive_tree( SAMPLE_DIR )
 SAMPLE_FILES = DICT_SAMPLE_DIR['file']
@@ -13,3 +14,6 @@ DICT_TEMPO_DIR = resource_loader.get_recursive_tree( TEMPO_DIR )
 TEMPO_FILES = sorted( DICT_TEMPO_DIR["file"] )
 
 ICON = resource_loader.get_icon('fps-sound-loopstation.png')
+
+KVSTRING_PC = VIEWS_DIR.joinpath('kvstring_pc.txt')
+KVSTRING_ANDROID = VIEWS_DIR.joinpath('kvstring_android.txt')
