b59ebfc69da42f4c29715f1cf750a5d15d7d887c
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-06-21 12:29:19 -0600

    kength

--- core/android_sound_info.py
+++ core/android_sound_info.py
@@ -1,10 +1,14 @@
 from jnius import autoclass
 
-MediaMetadataRetriever = autoclass("android.media.MediaMetadataRetriever")
+MediaExtractor = autoclass('android.media.MediaExtractor')
+MediaFormat = autoclass('android.media.MediaFormat')
 
 def get_audio_length(path: str) -> float:
-    retriever = MediaMetadataRetriever()
-    retriever.setDataSource(path)
-    duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
-    retriever.release()
-    return float(duration) / 1000.0 if duration else 0.0
+    extractor = MediaExtractor()
+    try:
+        extractor.setDataSource(path)
+        fmt = extractor.getTrackFormat(0)
+        duration_us = fmt.getLong(MediaFormat.KEY_DURATION)
+        return duration_us / 1_000_000.0
+    finally:
+        extractor.release()
