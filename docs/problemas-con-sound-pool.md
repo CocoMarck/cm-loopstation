98e8293faaccc75ea0faec0165861fc586a2961b
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-07-12 20:53:41 -0600

    Posiblemente sound pool troll o grabacion rarete.

---
+++ docs/problemas-con-sound-pool.md
@@ -0,0 +1,3 @@
+Al ser tan bajo nivel se tiene que configurar muy bien.
+
+`2026-07-12`: Error, al paracer no reproduce en su totolidad sonidos largos, esto esta muy mal. Descargar su uso hasta implementar soulución.
--- main.py
+++ main.py
@@ -54,7 +54,7 @@ metronome = DTMetronome(
 )
 loopstation = DTSoundLoopstation(
     dt_metronome=metronome,
-    sound_manager=sound_manager_android_pool_alone,
+    sound_manager=sound_manager_android,
     volume=config_engine.volume
 )
 microphone_recorder = AndroidMicrophoneRecorder()
